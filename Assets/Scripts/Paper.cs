b04719ed085ad61611c257e3b2b3b5514d30a609
zereyak13 <47759320+zereyak13@users.noreply.github.com>
2021-07-06 18:00:28 +0300

    Updated ToDo 0.1
    Liste 0.1 eklentileri..

--- Assets/Scripts/Paper.cs
+++ Assets/Scripts/Paper.cs
@@ -4,24 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Paper : MonoBehaviour
 {
+    [SerializeField] private GameObject sicrama;
+    [SerializeField] private int paperEffect;
+
     Animator playerAnimator;
+
 
     private void Start()
     {
         playerAnimator = GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<Animator>();
     }
 
-    [SerializeField] private int paperEffect;
-
     private void OnTriggerEnter(Collider other)
     {
-        if (InkBar.Instance.InkBarGO.GetComponent<Slider>().value != 0)//Sadece mürekkep varsa imza atar.
+        if (other.gameObject.CompareTag("Player"))
         {
-            transform.GetChild(0).gameObject.SetActive(true);
-            InkBar.Instance.SetInkBar(paperEffect);
-            playerAnimator.SetTrigger("forwardFlip");
-            NiceVibrationsCall.Instance.HeavyVibration();
+            if (InkBar.Instance.InkBarGO.GetComponent<Slider>().value != 0)//Sadece mürekkep varsa imza atar.
+            {
+                //stamped paper
+                transform.GetChild(0).gameObject.SetActive(true);
+                //Set Ink bar
+                InkBar.Instance.SetInkBar(paperEffect);
+                playerAnimator.SetTrigger("forwardFlip");
+                //Vibration
+                NiceVibrationsCall.Instance.HeavyVibration();
+                // sicrama Pos = this.gameobject.pos;
+                //sicrama.transform.position = transform.position;
+                ////Start animation
+                //sicrama.gameObject.GetComponent<Animator>().SetTrigger("paintSplash");
+
+                //Tek satýr haline getirilecek
+            }
         }
+
+
         //if (other.gameObject.CompareTag("Player"))
         //{
         //    StartCoroutine(AddDelayForStamp(0.190f));
