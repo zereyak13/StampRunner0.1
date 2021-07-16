5b8632e18ca382866708015c5d30ea89d026dbfb
zereyak13 <47759320+zereyak13@users.noreply.github.com>
2021-07-16 12:14:41 +0300

    Added: Elephant codes && gameLogo

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using ElephantSDK;
 
 public class SceneManagement : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class SceneManagement : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+
+        Elephant.LevelStarted(SceneManager.GetActiveScene().buildIndex);
     }
     public void LoadThisScene()
     {
@@ -26,17 +29,21 @@ public class SceneManagement : MonoBehaviour
 
         if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings -1)
         {
+            Elephant.LevelCompleted(SceneManager.GetActiveScene().buildIndex);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
         else
         {
+            Elephant.LevelCompleted(SceneManager.GetActiveScene().buildIndex);
             SceneManager.LoadScene(0);
         }
     }
     IEnumerator AddDelay2()
     {
         yield return new WaitForSeconds(2f);
+        Elephant.LevelCompleted(SceneManager.GetActiveScene().buildIndex);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
 
     public int GetThisLevel()
